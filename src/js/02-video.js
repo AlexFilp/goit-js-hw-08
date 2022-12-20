23db00a367d4b2d5e62525fc976e87874164c5c1
Oleksandr FIlippov <filya170895@gmail.com>
2022-12-20 13:25:45 +0100

    task 2 in progress

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,42 @@
 import Player from '@vimeo/player';
 
 console.log(Player);
+
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.on('pause', function () {
+  console.log('stoped the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+function onPlay(data) {
+  console.log(data);
+}
+
+player.on('timeupdate', onPlay);
+
+player
+  .setCurrentTime(30.456)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
