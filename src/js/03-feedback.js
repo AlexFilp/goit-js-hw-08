cc247372f087e2bdd0d82b34ad338463854bdd7f
Oleksandr FIlippov <filya170895@gmail.com>
2022-12-20 19:27:26 +0100

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,51 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+console.dir(form);
+
+const formData = {};
+
+const DATA_STORAGE = 'feedback-form-state';
+
+saveCurrentData();
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  evt.currentTarget.reset();
+
+  localStorage.removeItem(DATA_STORAGE);
+  user = {
+    email: formData.email,
+    message: formData.message,
+  };
+  console.log(user);
+}
+
+form.addEventListener('input', throttle(onTypeInput, 500));
+
+function onTypeInput(event) {
+  //   console.log(event.target.name);
+  //   console.log(event.target.value);
+
+  formData[event.target.name] = event.target.value;
+
+  localStorage.setItem(DATA_STORAGE, JSON.stringify(formData));
+}
+
+function saveCurrentData() {
+  const savedData = localStorage.getItem(DATA_STORAGE);
+
+  if (savedData) {
+    const parsedSaveData = JSON.parse(savedData);
+    console.log(savedData);
+    console.log(parsedSaveData);
+
+    form.email.value = parsedSaveData.email;
+
+    form.message.value = parsedSaveData.message;
+  }
+}
